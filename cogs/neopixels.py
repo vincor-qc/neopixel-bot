0f8b4f84c1de60074aab4d389fefe395fdad986f
Vincent <vincent.qi@hotmail.com>
2022-07-19 18:55:38 -0700

    fix: update default speeds again

--- cogs/neopixels.py
+++ cogs/neopixels.py
@@ -102,7 +102,7 @@ class Neopixels(commands.Cog):
         self.cancel_loop()
 
         self.is_looping = True
-        self.loop_task = asyncio.ensure_future(neopixels.rainbow_cycle(0.01 / self.speed))
+        self.loop_task = asyncio.ensure_future(neopixels.rainbow_cycle(0.1 / self.speed))
 
         await ctx.send("Rainbow cycle set.")
 
@@ -112,7 +112,7 @@ class Neopixels(commands.Cog):
         self.cancel_loop()
 
         self.is_looping = True
-        self.loop_task = asyncio.ensure_future(neopixels.rainbow_breathing(0.01 / self.speed))
+        self.loop_task = asyncio.ensure_future(neopixels.rainbow_breathing(0.005 / self.speed))
 
         await ctx.send("Rainbow breathing set.")
 
